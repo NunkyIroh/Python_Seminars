9ee8655ebf127a2505449a58e096f51bfee4f9e5
Uncle_Iroh <vadegtyarev@gmail.com>
2022-09-04 21:57:39 +0300

    Сделал задачу 2. Нахождение максимального числа

--- Seminar_1_Task_2.py
+++ Seminar_1_Task_2.py
@@ -5,3 +5,27 @@
 
 # - 1, 4, 8, 7, 5 -> 8
 # - 78, 55, 36, 90, 2 -> 90
+
+a = int(input('Введите первое число а = '))
+b = int(input('Введите второе число b = '))
+c = int(input('Введите третье число c = '))
+d = int(input('Введите четвертое число d = '))
+e = int(input('Введите пятое число e = '))
+
+max = a
+
+# if (b>max):
+#     max = b
+# if (c>max):
+#     max = c
+# if (d>max):
+#     max = d
+# if (e>max):
+#     max = e
+# print (max)
+
+numbers = [a, b, c, d, e]
+for i in numbers:
+    if (i>max):
+        max = i
+print (max)
