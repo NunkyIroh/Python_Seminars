0fbaefb991382b3d109c58b8ed36e7a102e85851
Uncle_Iroh <vadegtyarev@gmail.com>
2022-09-04 16:54:48 +0300

    Ввод в семинары

---
+++ Seminar_1_Task_0.py
@@ -0,0 +1,12 @@
+from base64 import b32decode
+
+
+a = 3
+b = 4
+c = a + b
+print(c, a*b)
+print(type(c))
+
+a = int(input('Введите первое число а = '))
+b = int(input('Введите первое число b = '))
+c = print('сумма введенных чисел равна ', a+b)
